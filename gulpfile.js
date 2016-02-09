eb10c6f4946f66937e06cf77f0e324d49a06f078
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-02-08 21:03:00 -0800

    fix gulpfile async issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,20 @@
 'use strict';
 
 var gulp = require('gulp');
-var plumber = require('gulp-plumber');
 var del = require('del');
 var rename = require('gulp-rename');
 
-// Gulp plumber error handler
-var onError = function(err) {
-	console.log(err);
-}
-
-gulp.task('clean', function(){
-	return del(['build/**/*']);
+gulp.task('clean', function(cb){
+	del(['build/**/*']).then(paths => cb())
 });
 
-gulp.task('build', function(){
-	return gulp.src(['./public/**/*'])
-		.pipe(plumber())
+gulp.task('build', ['clean'], function(cb){
+	gulp.src(['./public/**/*'])
 		.pipe(gulp.dest('./build'));
 });
 
-gulp.task('addIndex', function(){
-	return gulp.src(['./views/index.ejs'])
-		.pipe(plumber())
+gulp.task('addIndex', ['clean'], function(cb){
+	gulp.src(['./views/index.ejs'])
 		.pipe(rename('index.html'))
 		.pipe(gulp.dest('./build'));
 });
