c9c47d8ffc2183fb4d4d51ebb5338494c6e4b14c
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-02-12 10:36:30 -0800

    add newline

--- public/js/directives/time-data.js
+++ public/js/directives/time-data.js
@@ -71,6 +71,7 @@ angular
         });
 
         if (!data.length) return;
+        
         var filler = Array(300).fill({x: 0, y: 0}).map(function(e, i){
           return {x: data[0].x - (1000*i), y:0}
         }).reverse();
